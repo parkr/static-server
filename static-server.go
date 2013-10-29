a3f2dfbb654af6abb7ec43f31a0901c5ff5d4667
Parker Moore <parkrmoore@gmail.com>
2013-10-29 15:07:25 -0400

    Run gofmt

--- static-server.go
+++ static-server.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
-  Path string
-  Body []byte
+	Path string
+	Body []byte
 }
 
 func loadPage(path string) (*Page, error) {
-  body, err := ioutil.ReadFile(path)
-  if err != nil {
-    return nil, err
-  }
-  return &Page{Path: path, Body: body}, nil
+	body, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Path: path, Body: body}, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  page, err := loadPage(r.URL.Path[1:])
-  if err != nil {
-    fmt.Println((err))
-  }
-  fmt.Fprintf(w, "%s", page.Body)
+	page, err := loadPage(r.URL.Path[1:])
+	if err != nil {
+		fmt.Println((err))
+	}
+	fmt.Fprintf(w, "%s", page.Body)
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
